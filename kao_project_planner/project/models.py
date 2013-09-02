eeb28124de81fb39b7e6fce73f1423672718af75
Chris Loew <cloew123@gmail.com>
2013-09-01 23:14:15 -0600

    Added string representations of the project models

--- kao_project_planner/project/models.py
+++ kao_project_planner/project/models.py
@@ -4,17 +4,26 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     
+    def __unicode__(self):
+        return self.title
+    
 class Iteration(models.Model):
     title = models.CharField(max_length=200)
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     project = models.ForeignKey(Project)
     
+    def __unicode__(self):
+        return self.title
+    
 class UserStory(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project)
     iteration = models.ForeignKey(Iteration)
+    
+    def __unicode__(self):
+        return self.title
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -22,3 +31,6 @@ class Task(models.Model):
     time_estimate = models.IntegerField(default=0)
     time_used = models.IntegerField(default=0)
     user_story = models.ForeignKey(UserStory)
+    
+    def __unicode__(self):
+        return self.title
