5d3cef46bdc71ea1a3e34d0792818b7ac08f4e29
Chris Loew <cloew123@gmail.com>
2013-09-01 23:43:56 -0600

    Made the descriptions and the user story's iteration optional

--- kao_project_planner/project/models.py
+++ kao_project_planner/project/models.py
@@ -18,16 +18,16 @@ class Iteration(models.Model):
     
 class UserStory(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     project = models.ForeignKey(Project)
-    iteration = models.ForeignKey(Iteration)
+    iteration = models.ForeignKey(Iteration, blank=True, null=True)
     
     def __unicode__(self):
         return self.title
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     time_estimate = models.IntegerField(default=0)
     time_used = models.IntegerField(default=0)
     user_story = models.ForeignKey(UserStory)
